6f4c34c2c7027ead36f595c98db52a2686449eae
Darusia <dariapionk13@gmail.com>
2021-10-20 17:01:47 +0200

    Zadanie 6 (dziala)

--- lab03/zadanie6.py
+++ lab03/zadanie6.py
@@ -2,7 +2,7 @@ import pysftp
 
 komenda = input("Jaką komendę wybierasz? POBIERZ lub WYŚLIJ")
 
-if komenda != "POBIERZ" and komenda != "WYŚŁIJ":
+if komenda != "POBIERZ" and komenda != "WYŚLIJ":
     print("Błędna komenda")
 if komenda == "POBIERZ":
     sciezka = input("Podaj ścieżkę do pliku z serwera zdalnego")
@@ -12,6 +12,6 @@ if komenda == "POBIERZ":
 else: 
     sciezka = input("Podaj ścieżkę do pliku lokalnego")
     haslo = input("Podaj swoje hasło do serwera zdalnego")
-    with pysftp.Connection("sigma.ug.edu.pl", username = "dpionk", password="haslo") as sftp:
+    with pysftp.Connection("sigma.ug.edu.pl", username = "dpionk", password=haslo) as sftp:
         sftp.put(sciezka)
 print("Gotowe :)")
