770d12556eb278da3758e0fe96a61d7c3d24aee6
Darusia <dariapionk13@gmail.com>
2021-11-24 17:29:10 +0100

    Socket.io

--- lab07/ws/app.js
+++ lab07/ws/app.js
@@ -3,20 +3,32 @@ const connect = require("connect");
 const app = connect();
 const serveStatic = require('serve-static');
 const server = require("http").createServer(app);
+const io = require('socket.io')(server);
+
 const WebSocket = require("ws")
 const wss = new WebSocket.Server({ server })
 app.use(serveStatic("public"));
 
-wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(data) {
-        wss.clients.forEach(function each(client) {
-            if (client != ws && client.readyState == WebSocket.OPEN) {
-                client.send(data);
+// wss.on('connection', function connection(ws) {
+//     ws.on('message', function incoming(data) {
+//         wss.clients.forEach(function each(client) {
+//             if (client != ws && client.readyState == WebSocket.OPEN) {
+//                 client.send(data);
                 
-            }
-        })
+//             }
+//         })
+//     })
+// })
+
+io.on('connection', client => {
+    client.on('connect', data => {
+        console.log("Someone just connected")
+    })
+    client.on('disconnect', () => {
+        console.log("Someone just disconnected")
     })
 })
+
 server.listen(3000, function () {
     console.log('Serwer HTTP działa na porcie 3000');
 });
